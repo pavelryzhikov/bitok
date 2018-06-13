182b4a510d123f91a0cca2c12b84b37e2677217a
Pavel Ryzhikov <pavelryzhikov@gmail.com>
2018-06-13 10:31:29 +0000

    now bot get updates with webhook

--- my_app.py
+++ my_app.py
@@ -1,69 +1,72 @@
 # -*- coding: utf-8 -*-
 from flask import Flask, request
-import logging
+import logging, time
 import config
 import requests
-print(2)
 app = Flask(__name__)
 TOKEN = config.TOKEN
-print(3)
 logging.basicConfig(filename='log_'+__name__+'.log', filemode='w', level=logging.DEBUG)
-#logging.warning('And this, too')
 
 @app.route('/')
 def hello_world():
     logging.info('hello_world')
-    return 'Hello, World!'
+    return "<h1 style='color:blue'>Hello There Rpe!</h1>"
+
+@app.route('/time')
+def hello_time():
+    return "<h1 style='color:blue'>Hello There " + time.ctime() + "</h1>"
+
 
 
 @app.route('/'+TOKEN+'/token')
 def token():
-    logging.info('token')
     return 'token'
+
+
 
 @app.route('/'+TOKEN+'/webhook/activate')
 def WebhookActivate():
+    """
+it does not works for own servers
+only for pythonanywhere
+
+use curl from 
+https://stackoverflow.com/questions/33256898/telegram-bot-on-openssl/33260827#33260827
+    """
     r = requests.post(url='https://api.telegram.org/bot'+config.TOKEN+'/setWebhook',
-            data ={ 'url': 'https://'+config.SERVER_URL+':'+config.SERVER_PORT+'/'+config.TOKEN+'/getupdates'
-                ,'certificate':''
-                #,'certificate': open('webhook_cert.pem', 'rb')
+            data ={ 'url': 'https://'+config.SERVER_URL+'/'+config.TOKEN
+                ,'certificate': open('/etc/nginx/ssl/YOURPUBLIC.pem', 'rb')
                 })
     logging.info(r.text)
-    print(r.text)
     return r.text
 
 @app.route('/'+TOKEN+'/webhook/deactivate')
 def WebhookDeactivate():
     r = requests.post(url='https://api.telegram.org/bot'+config.TOKEN+'/setWebhook')
-    logging.info(r.text)
-    print(r.text)
     return r.text
 
 @app.route('/'+TOKEN+'/webhook/info')
 def getWebhookInfo():
     r = requests.get(url='https://api.telegram.org/bot'+config.TOKEN+'/getWebhookInfo')
-    logging.info(r.text)
-    print(r.text)
     return r.text
 
-@app.route('/'+TOKEN+'/getupdates', methods=['POST'])
+@app.route('/'+TOKEN, methods=['POST'])
 def getUpdates():
-    logging.info('CALL GETUPADTES')
     try:
         logging.info(request.data)
     except:
-        logging.error(request.data)
+        logging.error('ERROR!!!')
         pass
     return 'OK', 200
 
+@app.route('/'+TOKEN+'/getupdates1', methods=['GET'])
+def getUpdates1():
+    try:
+        r = requests.post(url='https://api.telegram.org/bot'+config.TOKEN+'/getUpdates')
+    except:
+        pass
+    return r.text, 200
+
 if __name__ == '__main__':
-#    app = Flask(__name__)
-#    from my_app import app
-    print(1)
-    app.run(
-            host=config.SERVER_URL
-            ,port=config.SERVER_PORT
-            #,ssl_context='adhoc'
-            #,ssl_context=('webhook_cert.pem', 'webhook_pkey.pem')
-            )
+    app.run(host='0.0.0.0')
 
